16c44757c27bb258e01bfbc0410f3a4f8b72f711
Devin Weaver <suki@tritarget.org>
2010-10-26 12:23:14 -0400

    Adds unit test. Competes model

--- app/models/equipment_asset.rb
+++ app/models/equipment_asset.rb
@@ -1,3 +1,5 @@
 class EquipmentAsset < ActiveRecord::Base
   unloadable
+
+  validates_presence_of :name
 end
--- test/unit/equipment_asset_test.rb
+++ test/unit/equipment_asset_test.rb
@@ -4,7 +4,13 @@ class EquipmentAssetTest < ActiveSupport::TestCase
   fixtures :equipment_assets
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  context "EquipmentAsset model" do
+    setup do
+      assert @asset = EquipmentAsset.new
+    end
+
+    should "not save without a title" do
+      assert !@asset.save, "model saved without a title"
+    end
   end
 end
