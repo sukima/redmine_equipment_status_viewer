1d0a67e9ee6e2eb7c8675f1c6a0b420773ef21b6
Devin Weaver <suki@tritarget.org>
2010-12-21 11:35:07 -0500

    Adds permissions

--- init.rb
+++ init.rb
@@ -8,6 +8,10 @@ Redmine::Plugin.register :redmine_equipment_status_viewer do
   url 'http://github.com/sukima/RedmineEquipmentStatusPlugin'
   author_url 'http://github.com/sukima'
 
+  permission :equipment_assets, {:equipment_assets => [:destroy, :update, :create, :edit, :new]}
+  permission :equipment_assets, {:equipment_assets => [:index, :show]}, :public => true
+  permission :asset_check_ins, {:asset_check_ins => [:new, :create]}, :public => true
+
   menu :application_menu, :equipment_status_viewer,
     { :controller => 'equipment_assets', :action => 'index' },
     :caption => 'Equipment'
