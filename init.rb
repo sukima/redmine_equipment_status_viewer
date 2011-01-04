57d81e00e99859e28aabaef554e3a06cd03c3cc7
Devin Weaver <suki@tritarget.org>
2011-01-04 11:37:57 -0500

    Bumps version to 0.2.2

--- init.rb
+++ init.rb
@@ -29,7 +29,7 @@ Redmine::Plugin.register :redmine_equipment_status_viewer do
   name 'Redmine Equipment Status Viewer plugin'
   author 'Devin Weaver'
   description 'Allows admins to make a list of equipment and track if they are inservice or not'
-  version '0.2.1'
+  version '0.2.2'
   url 'http://github.com/sukima/RedmineEquipmentStatusPlugin'
   author_url 'http://github.com/sukima'
 
