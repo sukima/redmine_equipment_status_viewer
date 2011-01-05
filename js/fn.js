52b8422f8d4492f500be47b3c4f5345fe348fa5e
Devin Weaver <suki@tritarget.org>
2011-01-04 22:31:21 -0500

    Auto scrolls top banner

--- js/fn.js
+++ js/fn.js
@@ -2,11 +2,33 @@ $.facebox.settings.closeImage = 'css/images/closelabel.png'
 $.facebox.settings.loadingImage = 'css/images/loading.gif'
 
 
+function mycarousel_initCallback(carousel)
+{
+    // Disable autoscrolling if the user clicks the prev or next button.
+    carousel.buttonNext.bind('click', function() {
+        carousel.startAuto(0);
+    });
+
+    carousel.buttonPrev.bind('click', function() {
+        carousel.startAuto(0);
+    });
+
+    // Pause autoscrolling if the user moves with the cursor over the clip.
+    carousel.clip.hover(function() {
+        carousel.stopAuto();
+    }, function() {
+        carousel.startAuto();
+    });
+};
+
+
 jQuery(document).ready(function() {
 
   jQuery('#mycarousel').jcarousel({
     wrap: "both",
+    auto: 10,
     scroll: 1, 
+    initCallback: mycarousel_initCallback,
     animation: "slow"
   });
 
