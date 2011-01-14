0e95d08cc974404d97de2277f69fd5c490423d05
Devin Weaver <suki@tritarget.org>
2011-01-14 08:42:14 -0500

    Moves URL to github pages

--- init.rb
+++ init.rb
@@ -30,7 +30,7 @@ Redmine::Plugin.register :redmine_equipment_status_viewer do
   author 'Devin Weaver'
   description 'Allows admins to make a list of equipment and track if they are inservice or not'
   version '0.3.2'
-  url 'http://github.com/sukima/redmine_equipment_status_viewer'
+  url 'http://sukima.github.com/redmine_equipment_status_viewer'
   author_url 'http://github.com/sukima'
 
   permission :view_equipment_assets, {:equipment_assets => [:index, :show, :print]}
