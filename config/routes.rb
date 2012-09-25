d54f3455eda2801a2760ca463133ae91606dc4f7
Devin Weaver <suki@tritarget.org>
2012-09-25 15:22:12 -0400

    Fixes missing print routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ RedmineApp::Application.routes.draw do
   # Redmine-2.1 uses Rails 3 which has a new routing DSL.
   # Info found from http://www.engineyard.com/blog/2010/the-lowdown-on-routes-in-rails-3/
   resources :equipment_assets do
+    member do
+      get :print
+    end
+    collection do
+      put :print
+    end
     resources :asset_check_ins, :only => [:new, :create] do
       member do
         post :new
         get :loclist
       end
-      match "check_in", :to => "asset_check_ins#new"
+      # match "check_in", :to => "asset_check_ins#new", :as => "check_in"
     end
   end
-  match "/ci/:equipment_assets", :to => "asset_check_ins#new"
+  match "/ci/:equipment_assets", :to => "asset_check_ins#new", :as => "equipment_asset_check_in"
 end
